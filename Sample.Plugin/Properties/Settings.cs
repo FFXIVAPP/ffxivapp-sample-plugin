a14b0b8f7d56f8ce6293b44a2a12d3231751a79a
Ryan Wilson <syndicated.life@gmail.com>
2014-05-02 13:40:50 -0700

    initial commit

---
+++ Sample.Plugin/Properties/Settings.cs
@@ -0,0 +1,238 @@
+﻿// Sample.Plugin
+// Settings.cs
+// 
+// Copyright © 2007 - 2014 Ryan Wilson - All Rights Reserved
+// 
+// Redistribution and use in source and binary forms, with or without 
+// modification, are permitted provided that the following conditions are met: 
+// 
+//  * Redistributions of source code must retain the above copyright notice, 
+//    this list of conditions and the following disclaimer. 
+//  * Redistributions in binary form must reproduce the above copyright 
+//    notice, this list of conditions and the following disclaimer in the 
+//    documentation and/or other materials provided with the distribution. 
+//  * Neither the name of SyndicatedLife nor the names of its contributors may 
+//    be used to endorse or promote products derived from this software 
+//    without specific prior written permission. 
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+// POSSIBILITY OF SUCH DAMAGE. 
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows.Media;
+using System.Xml.Linq;
+using FFXIVAPP.Common.Helpers;
+using FFXIVAPP.Common.Models;
+using FFXIVAPP.Common.Utilities;
+using NLog;
+using Color = System.Windows.Media.Color;
+using ColorConverter = System.Windows.Media.ColorConverter;
+using FontFamily = System.Drawing.FontFamily;
+
+namespace Sample.Plugin.Properties
+{
+    internal class Settings : ApplicationSettingsBase, INotifyPropertyChanged
+    {
+        private static Settings _default;
+
+        public static Settings Default
+        {
+            get { return _default ?? (_default = ((Settings) (Synchronized(new Settings())))); }
+        }
+
+        public override void Save()
+        {
+            // this call to default settings only ensures we keep the settings we want and delete the ones we don't (old)
+            DefaultSettings();
+            SaveSettingsNode();
+            // I would make a function for each node itself; other examples such as log/event would showcase this
+            Constants.XSettings.Save(Path.Combine(FFXIVAPP.Common.Constants.PluginsSettingsPath, "Sample.Plugin.xml"));
+        }
+
+        private void DefaultSettings()
+        {
+            Constants.Settings.Clear();
+        }
+
+        public new void Reset()
+        {
+            DefaultSettings();
+            foreach (var key in Constants.Settings)
+            {
+                var settingsProperty = Default.Properties[key];
+                if (settingsProperty == null)
+                {
+                    continue;
+                }
+                var value = settingsProperty.DefaultValue.ToString();
+                SetValue(key, value, CultureInfo.InvariantCulture);
+                RaisePropertyChanged(key);
+            }
+        }
+
+        public void SetValue(string key, string value, CultureInfo cultureInfo)
+        {
+            try
+            {
+                var type = Default[key].GetType()
+                                       .Name;
+                switch (type)
+                {
+                    case "Boolean":
+                        Default[key] = Boolean.Parse(value);
+                        break;
+                    case "Color":
+                        var cc = new ColorConverter();
+                        var color = cc.ConvertFrom(value);
+                        Default[key] = color ?? Colors.Black;
+                        break;
+                    case "Double":
+                        Default[key] = Double.Parse(value, cultureInfo);
+                        break;
+                    case "Font":
+                        var fc = new FontConverter();
+                        var font = fc.ConvertFromString(value);
+                        Default[key] = font ?? new Font(new FontFamily("Microsoft Sans Serif"), 12);
+                        break;
+                    case "Int32":
+                        Default[key] = Int32.Parse(value, cultureInfo);
+                        break;
+                    default:
+                        Default[key] = value;
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                Logging.Log(LogManager.GetCurrentClassLogger(), "", ex);
+            }
+        }
+
+        #region Property Bindings (Settings)
+
+        [UserScopedSetting]
+        [DebuggerNonUserCode]
+        [DefaultSettingValue("#FF000000")]
+        public Color ChatBackgroundColor
+        {
+            get { return ((Color) (this["ChatBackgroundColor"])); }
+            set
+            {
+                this["ChatBackgroundColor"] = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        [UserScopedSetting]
+        [DebuggerNonUserCode]
+        [DefaultSettingValue("#FF800080")]
+        public Color TimeStampColor
+        {
+            get { return ((Color) (this["TimeStampColor"])); }
+            set
+            {
+                this["TimeStampColor"] = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        [UserScopedSetting]
+        [DebuggerNonUserCode]
+        [DefaultSettingValue("Microsoft Sans Serif, 12pt")]
+        public Font ChatFont
+        {
+            get { return ((Font) (this["ChatFont"])); }
+            set
+            {
+                this["ChatFont"] = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        [UserScopedSetting]
+        [DebuggerNonUserCode]
+        [DefaultSettingValue("100")]
+        public Double Zoom
+        {
+            get { return ((Double) (this["Zoom"])); }
+            set
+            {
+                this["Zoom"] = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region Implementation of INotifyPropertyChanged
+
+        public new event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        private void RaisePropertyChanged([CallerMemberName] string caller = "")
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(caller));
+        }
+
+        #endregion
+
+        #region Iterative Settings Saving
+
+        private void SaveSettingsNode()
+        {
+            if (Constants.XSettings == null)
+            {
+                return;
+            }
+            var xElements = Constants.XSettings.Descendants()
+                                     .Elements("Setting");
+            var enumerable = xElements as XElement[] ?? xElements.ToArray();
+            foreach (var setting in Constants.Settings)
+            {
+                var element = enumerable.FirstOrDefault(e => e.Attribute("Key")
+                                                              .Value == setting);
+                if (element == null)
+                {
+                    var xKey = setting;
+                    var xValue = Default[xKey].ToString();
+                    var keyPairList = new List<XValuePair>
+                    {
+                        new XValuePair
+                        {
+                            Key = "Value",
+                            Value = xValue
+                        }
+                    };
+                    XmlHelper.SaveXmlNode(Constants.XSettings, "Settings", "Setting", xKey, keyPairList);
+                }
+                else
+                {
+                    var xElement = element.Element("Value");
+                    if (xElement != null)
+                    {
+                        xElement.Value = Default[setting].ToString();
+                    }
+                }
+            }
+        }
+
+        #endregion
+    }
+}
